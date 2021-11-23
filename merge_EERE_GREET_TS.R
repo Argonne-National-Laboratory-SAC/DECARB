eec859634bd4f4772ed5b4e5ace3ccfd975457d2
Kar <skar@anl.gov>
2021-11-23 17:46:21 -0600

    caliberate values from LHV to HHV

--- merge_EERE_GREET_TS.R
+++ merge_EERE_GREET_TS.R
@@ -7,6 +7,7 @@ library(writexl)
 fpath <- "C:\\Users\\skar\\Box\\saura_self\\Proj - EERE Decarbonization"
 #f_elec <- "EERE_scenarios_TS_GREET1_all_summary_v2.xlsx"
 f_elec <- "EERE_scenarios_TS_GREET1_all_summary_v2 - EnergyUsePowerPlantConstMaterial=YES.xlsx"
+f_hvs <- "GREETheatingValues.xlsx"
 
 f_eere <- "EERE Tool_v12_gzz.xlsx"
 
@@ -14,11 +15,13 @@ fr_elec <- "Sheet1"
 fr_eere <- "GREET LCI!A4:J20557"
 fr_eere_names <- "Temp_Corr_EF_list"
 #fr_eere_elecPower <- "Electric Power"
+fr_hvs <- "mappings"
 
 d_elec <- read_excel(paste0(fpath,"\\", f_elec), sheet = fr_elec, na = c("NA", "Na", "na"))
 d_eere <- read_excel(paste0(fpath,"\\", f_eere), range = fr_eere, na = c("NA", "Na", "na"))
 d_eere_names <- read_excel(paste0(fpath,"\\", f_eere), sheet = fr_eere_names, na = c("NA", "Na", "na")) %>%
   distinct()
+d_hvs <- read_excel(paste0(fpath,"\\", f_hvs), sheet = fr_hvs, na = c("NA", "Na", "na", " "))
 
 #d_eere_elecPower <- read_excel(paste0(fpath,"\\", f_eere), sheet = fr_eere_elecPower, skip = 3, na = c("NA", "Na", "na")) %>%
 #  filter(Subsector %in% c("Electric Power Sector")) %>%
@@ -43,8 +46,14 @@ d <- d_eere %>%
                            "Year", 
                           "Scope" = "LC Phase")) %>%
   mutate(BAU = round(BAU, 3), Elec0 = round(Elec0, 3), Elec_CI_depend_frac = round(Elec_CI_depend_frac, 6)) %>%
-  distinct()
-
+  distinct() %>%
+  left_join(d_hvs, by = c("GREET Pathway" = "EERE_pathway")) %>%
+  select(!c(GREET_Fuel)) %>%
+  mutate(LHV_by_HHV = as.numeric(LHV_by_HHV)) %>%
+  #group_by(Index) %>%
+  mutate(BAU_HHV =  BAU * LHV_by_HHV,
+         Elec0_HHV = Elec0 * LHV_by_HHV)
 
 #write_xlsx(d, paste0(fpath, "\\", 'GREET_LCI_forEEREtool_mmBtu.xlsx'))
 write_xlsx(d, paste0(fpath, "\\", 'GREET_LCI_forEEREtool_mmBtu - EnergyUsePowerPlantConstMaterial=YES.xlsx'))
+
