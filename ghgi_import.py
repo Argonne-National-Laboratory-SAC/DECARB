5d57e63c397f4b9263bfc8217be20871a1ded8fd
Kar <skar@anl.gov>
2022-02-02 17:19:10 -0600

    encapsulating calculations and data loading to a class

--- ghgi_import.py
+++ ghgi_import.py
@@ -17,72 +17,74 @@ import numpy as np
 import seaborn as sns
 
 #%%
-#Load in GHGI Data
 
-# Set filepath to GHGI Data
-filepath = 'C:/Users/gzaimes/Desktop/EPA GHGI/Input Data/'
+class EPA_GHGI_import:
+    
+    def __init__(self):
+        
+        # Load in GHGI Data
 
-# Load in GHGI import sheet
-df = pd.read_excel(filepath + 'ghgi_correspondence.xlsx')
+        # Set filepath to GHGI Data
+        self.filepath = 'C:/Users/skar/Box/saura_self/Proj - EERE Decarbonization/data/EPA GHGI Input Data/'
+        
+        # Load in GHGI import sheet
+        df = pd.read_excel(self.filepath + 'ghgi_correspondence.xlsx')
+        
+        # create list to append GHGI data
+        temp_list = [] 
+        
+        # Loop through data tables in GHGI
+        for row in df.itertuples():
+            print('Currently fetching: ' + row.filename)
+            df_temp = pd.read_excel(self.filepath + "ghgi data tables/" + str(row.filename) + ".xlsx", sheet_name='Tidy')
+            df_temp['Table'] = row.filename
+            temp_list.append(df_temp)    
+        
+        self.df_ghgi = pd.concat(temp_list, axis=0)
+        self.df_ghgi = self.df_ghgi.reset_index(drop=True)
+        
+        # Load in 100-Yr GWP Factors
+        self.lcia = pd.read_excel(self.filepath + 'gwp factors.xlsx', sheet_name='Tidy')
 
-#create list to append GHGI data
-temp_list = [] 
+        # Use AR4 100-Yr GWP Factors, so that results can be compared with EIA's GHGI.         
+        lcia_ar4 = self.lcia[self.lcia['LCIA Method'] == 'AR4'].copy()
+        lcia_ar4['GWP_years'] = 100
+        
 
-# Loop through data tables in GHGI
-for row in df.itertuples():
-    print(row.filename)
-    df_temp = pd.read_excel(filepath + "ghgi data tables/" + str(row.filename) + ".xlsx", sheet_name='Tidy')
-    df_temp['Table'] = row.filename
-    temp_list.append(df_temp)    
+        # Process GHGI Data
+        
+        # Convert carbon flows (C) to Carbon Dioxide (CO2), e.g. C --> CO2
+        c_to_co2 = (16*2+12)/12
+        
+        # Replace non-numeric values reported in GHGI
+        self.df_ghgi.loc[~ self.df_ghgi["Value"].apply(np.isreal), 'Value'] = 0 
+        
+        # Update GHGI C FLows --> CO2
+        self.df_ghgi.loc[self.df_ghgi['Emissions Type']=='C','Value'] = self.df_ghgi.loc[self.df_ghgi['Emissions Type']=='C','Value'] * c_to_co2
+        self.df_ghgi.loc[self.df_ghgi['Emissions Type']=='C', 'Emissions Type'] = 'CO2'
+        
+        # Merge GWP factors to dataframe
+        self.df_ghgi = self.df_ghgi.merge(lcia_ar4, how='left', on=['Emissions Type'])
 
-df_ghgi = pd.concat(temp_list, axis=0)
-df_ghgi = df_ghgi.reset_index(drop=True)
+        # Unit conversion (all values relative to MMmt)
+        unit_conv = {'kt': 10**-3,
+                     'mt': 10**-6,
+                     'MMmt': 1
+                     }
+        
+        # Map unit conversions, and calculate GHG Emissions (MMmt CO2e)
+        self.df_ghgi['Unit Conv'] = self.df_ghgi['Unit'].map(unit_conv)
+        self.df_ghgi['GHG Emissions'] = self.df_ghgi['Value']  * self.df_ghgi['GWP'] * self.df_ghgi['Unit Conv']
 
-#Remove unnecessary varibles
-del temp_list, df_temp, row, df
+        #Aggregate results by Year, Sector, and Source
+        self.df_ghgi_agg = self.df_ghgi.groupby(['Year', 'Inventory Sector', 'Economic Sector', 'Source'], as_index = False)['GHG Emissions'].sum()
+                
+        
+    # QA/QC Check
+    
+    def check_with_table_2_10(self):
+        return 1
 
-#%%
-#Load in 100-Yr GWP Factors
-
-lcia = pd.read_excel(filepath + 'gwp factors.xlsx', sheet_name='Tidy')
-
-#Use AR4 100-Yr GWP Factors, so that results can be compared with EIA's GHGI. 
-#NOTE - WE SHOULD ADD GWP TIME HORIZON AS SEPARATE COL**
-
-lcia_ar4 = lcia[lcia['LCIA Method'] == 'AR4']
-
-#%%
-#Process GHGI Data
-
-#Convert carbon flows (C) to Carbon Dioxide (CO2), e.g. C --> CO2
-c_to_co2 = (16*2+12)/12
-
-#Replace non-numeric values reported in GHGI
-df_ghgi.loc[~df_ghgi["Value"].apply(np.isreal), 'Value'] = 0 
-
-#Update GHGI C FLows --> CO2
-df_ghgi.loc[df_ghgi['Emissions Type']=='C','Value'] = df_ghgi.loc[df_ghgi['Emissions Type']=='C','Value'] * c_to_co2
-df_ghgi.loc[df_ghgi['Emissions Type']=='C', 'Emissions Type'] = 'CO2'
-
-#Merge GWP factors to dataframe
-df_ghgi = df_ghgi.merge(lcia_ar4, how='left', on=['Emissions Type'])
-
-#Unit conversion (all values relative to MMmt)
-unit_conv = {'kt': 10**-3,
-             'mt': 10**-6,
-             'MMmt': 1
-             }
-
-#Map unit conversions, and calculate GHG Emissions (MMmt CO2e)
-df_ghgi['Unit Conv'] = df_ghgi['Unit'].map(unit_conv)
-df_ghgi['GHG Emissions'] = df_ghgi['Value']  * df_ghgi['GWP'] * df_ghgi['Unit Conv']
-
-#Aggregate Results by Sector and Source
-df_ghgi_agg = df_ghgi.groupby(['Year', 'Inventory Sector', 'Economic Sector', 'Source'], as_index = False)['GHG Emissions'].sum()
-
-#Remove unnecessary varibles
-del lcia_ar4, unit_conv, c_to_co2
-
-#%%
-# QA/QC Check
+if __name__ == "__main__":
+    ob1 = EPA_GHGI_import()
 
