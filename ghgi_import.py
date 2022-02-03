ef00c11cdea101073b7ce79f5e497ac1016eca0e
Kar <skar@anl.gov>
2022-02-03 13:29:54 -0600

    add function for QA with Table 2.10

--- ghgi_import.py
+++ ghgi_import.py
@@ -75,16 +75,60 @@ class EPA_GHGI_import:
         # Map unit conversions, and calculate GHG Emissions (MMmt CO2e)
         self.df_ghgi['Unit Conv'] = self.df_ghgi['Unit'].map(unit_conv)
         self.df_ghgi['GHG Emissions'] = self.df_ghgi['Value']  * self.df_ghgi['GWP'] * self.df_ghgi['Unit Conv']
-
+        
         #Aggregate results by Year, Sector, and Source
         self.df_ghgi_agg = self.df_ghgi.groupby(['Year', 'Inventory Sector', 'Economic Sector', 'Source'], as_index = False)['GHG Emissions'].sum()
                 
+     
+    def remove_categories(self):
         
+        # Removing combustion based emissions
+        mask = (self.df_ghgi['Source'].str.contains('combustion', case=False, na=False))
+        self.df_ghgi = self.df_ghgi [~ mask]
+    
     # QA/QC Check
     
-    def check_with_table_2_10(self):
-        return 1
+    def QA_with_table_2_10(self, yr_filter = [2019] ):
+        
+        self.data_2_10_agg = self.table_2_10()
+        
+        self.data_oth = self.df_ghgi[self.df_ghgi['Table'] != 'Table 2-10'].copy()
+        self.data_oth_agg = self.data_oth.groupby(['Year', 'Economic Sector'], as_index = False)['GHG Emissions'].sum()
+        
+        d1 = self.data_oth_agg.merge(self.data_2_10_agg, left_on=['Year', 'Economic Sector'], right_on=['Year', 'Economic Sector'], how = 'left')
+        d1.rename(columns = {'GHG Emissions_x' : 'GHG Emissions_compiled', 'GHG Emissions_y' : 'GHG Emissions_2_10'}, inplace = True)
+        
+        d1['Difference by'] = d1['GHG Emissions_compiled'] - d1['GHG Emissions_2_10']
+        d1['perc diff'] = ( d1['GHG Emissions_compiled'] - d1['GHG Emissions_2_10'] ) / d1['GHG Emissions_2_10'] * 100
+        
+        d1 = d1[d1['Year'].isin(yr_filter)]
+        
+        return (d1)
+        
+    def table_2_10(self):
+                
+        df_temp = pd.read_excel(self.filepath + "ghgi data tables/" + "Table 2-10.xlsx", sheet_name='Table 2-10', header = 2, index_col = None)
+        df_temp = pd.melt(df_temp, id_vars=['Sector/Source'])
+        df_temp = df_temp[ ~ df_temp['Sector/Source'].isnull() ]
+        df_temp = df_temp[ ~ df_temp['value'].isnull() ]
+        df_temp = df_temp[ ~ df_temp['variable'].isin(['Percent a']) ]
+        df_temp.loc[ ~ df_temp['value'].apply(np.isreal), 'value' ] = 0
+        
+        df_temp = df_temp[ df_temp['Sector/Source'].isin(['Transportation', \
+                       'Electric Power Industry', 'Industry', 'Agriculture', 'Commercial', 'Residential', \
+                       'LULUCF Sector Net Total b' ]) ]
+        df_temp.rename(columns = {'Sector/Source' : 'Economic Sector', \
+                                  'variable' : 'Year', 'value' : 'GHG Emissions'}, inplace = True)
+        df_temp.loc[df_temp['Economic Sector'] == 'LULUCF Sector Net Total b', 'Economic Sector'] = 'LULUCF'
+        df_temp.loc[df_temp['Economic Sector'] == 'Electric Power Industry', 'Economic Sector'] = 'Electric Power'
+        
+        #self.df_temp = df_temp
+        return (df_temp)
+        
+        
 
 if __name__ == "__main__":
     ob1 = EPA_GHGI_import()
+    #ob1.table_2_10()
+    df = ob1.QA_with_table_2_10()
 
