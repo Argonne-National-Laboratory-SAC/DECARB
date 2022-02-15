35cbadfabf44a721f93c527502e4ce3569e388cf
Kar <skar@anl.gov>
2022-02-15 07:32:09 -0600

    improve unit conversion in industrial

--- unit_conversions.py
+++ unit_conversions.py
@@ -29,40 +29,35 @@ import pandas as pd
 
 class model_units:
     
-    def __init__(self):
-               
+    def __init__(self):               
         self.path_data = 'C:\\Users\\skar\\Box\\saura_self\\Proj - EERE Decarbonization\\data'
-        self.f_unit_conv = 'Unit Conversion.xlsx'
+        self.f_unit_convert = 'Unit Conversion.xlsx'
+        self.f_tool_units = 'EERE_tool_unit_conventions.xlsx'
         sheet_physical = 'physical'
         sheet_feedstock = 'feedstock'
         sheet_heatingvalues = 'HHV_to_LHV'
+        sheet_unit_conventions = 'unit_conventions'
         
-        physical = pd.read_excel(self.path_data + '\\' + self.f_unit_conv, sheet_name = sheet_physical)
-        feedstock = pd.read_excel(self.path_data + '\\' + self.f_unit_conv, sheet_name = sheet_feedstock)
-        heatingvalues = pd.read_excel(self.path_data + '\\' + self.f_unit_conv, sheet_name = sheet_heatingvalues)
+        physical = pd.read_excel(self.path_data + '\\' + self.f_unit_convert, sheet_name = sheet_physical)
+        feedstock = pd.read_excel(self.path_data + '\\' + self.f_unit_convert, sheet_name = sheet_feedstock)
+        heatingvalues = pd.read_excel(self.path_data + '\\' + self.f_unit_convert, sheet_name = sheet_heatingvalues)
+        eere_tool_units = pd.read_excel(self.path_data + '\\' + self.f_tool_units, sheet_name = sheet_unit_conventions)
         
         self.df_units = pd.concat([physical, feedstock, heatingvalues])
         
-        self.df_units['unit_conv'] = self.df_units['Convert_To'] + '_per_' + self.df_units['Convert_From']
+        self.df_units['unit_conv'] = self.df_units['Convert_To'] + '_per_' + self.df_units['Convert_From']  
         
         # Data frames to dictionaries        
         self.dict_units = self.df_units.set_index('unit_conv').to_dict()['Multiply_By']
-               
-        # Unit conventions
-        
-        self.to_units = {
-            'Energy' : 'MMBtu', # Million Metric British Thermal Unit
-            'Electricity' : 'TWh', # Terawatt Hour
-            'Biomass_Feedstock' : 'MT' # Metric Ton
-            }
-        
+        self.eere_tool_units = eere_tool_units.set_index('Category').to_dict()['Unit']
+                
     def select_units(self, ut):
         try:            
-            unit_category = self.df_units.loc[self.df_units['Convert_To'] == ut, 'Category'][0]
-            return self.to_units[unit_category]
+            unit_category = self.df_units.loc[self.df_units['Convert_From'] == ut, 'Category'].iloc[0]
+            return self.eere_tool_units[unit_category]
         
         except (Exception) as e:            
-            message1 = 'Possibly a unit key: ' + ut + ' not found.'
+            message1 = 'The unit key: ' + ut + ' not found.'
             print(message1)
             template = "An exception of type {0} occurred. Arguments:\n{1!r}"
             message2 = template.format(type(e).__name__, e.args)
@@ -72,8 +67,8 @@ class model_units:
         if convert in self.dict_units:
             return self.dict_units[convert]
         else:
-            print('Note: the requested unit conversion value is not found, returning 1. \
-                  Please check the Unit Conversion table to check value and maintain consistency.')
+            print('Note: the requested unit conversion value is not found, now returning 1 as multiplier. \
+                  Please check the Unit Conversion table to revise and maintain consistency.')
             return 1
 
     
